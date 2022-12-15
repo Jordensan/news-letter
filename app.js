72be2e4d20d866c31881817d995b63201c437131
unknown <jordensan1@gmail.com>
2022-12-15 12:32:46 -0500

    first

---
+++ app.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+
+const app = express();
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res) {
+  const fName = req.body.firstName;
+  const lName = req.body.lastName;
+  const email = req.body.email;
+  console.log(fName);
+  console.log(lName);
+  console.log(email);
+
+  const data = {
+    members: [{
+      email_address: email,
+      status: "subscribed",
+      merge_fields: {
+        FNAME: fName,
+        LNAME: lName
+      }
+    }]
+  };
+
+  const jsonData = JSON.stringify(data);
+  const url = process.env.URL;
+  const options = {
+    method: "POST",
+    auth: process.env.AUTH
+  }
+
+  const request = https.request(url, options, function(response) {
+    response.on("data", function(data) {
+      console.log(JSON.parse(data));
+      var statCode = response.statusCode;
+      console.log('response.code: ', statCode);
+      if (statCode == 200) {
+        res.sendFile(__dirname + "/success.html");
+      } else {
+
+        res.sendFile(__dirname + "/failure.html");
+      }
+    })
+  })
+  request.write(jsonData);
+  request.end();
+
+});
+
+app.post("/failure", function(req, res) {
+  res.redirect("/");
+})
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log("New server up!");
+});
+
+
+
+
+//TO UPDATE
+
+/*
+git add .
+git commit -m ""
+git push heroku master
+*/
